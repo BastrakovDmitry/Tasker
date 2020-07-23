9d36ef69e0a0ce35eddb0338d3a0c568d94a8b5b
Bastrakov <Diman199721@gmail.com>
2020-07-23 15:52:08 +0400

    Commit

---
+++ src/ru/Bastrakov/Tasker/Main.java
@@ -0,0 +1,11 @@
+package ru.Bastrakov.Tasker;
+
+import ru.Bastrakov.Tasker.GUI.GUIFrame;
+
+public class Main {
+    public static GUIFrame frame;
+
+    public static void main(String[] args) {
+        frame = new GUIFrame();
+    }
+}
